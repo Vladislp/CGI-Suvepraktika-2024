224c79a1e28c33e888d93770097f24806a9ea863
Vladislp <42935979+Vladislp@users.noreply.github.com>
2024-03-05 11:17:22 +0200

    FEATURE-8-Need to get access to more event properties
    1) Comment code
    2) Adding new variables, for acquiring XML properties to use them in our application.
    3) Creating new Modal(Class) for Image presentation. Because it's not a simple string, but complex data (object). Then we use it with "event.Images.EventMediumImagePortrait

--- Backend/Spring/demo/src/main/java/com/example/demo/cinema/controller/CinemaController.java
+++ Backend/Spring/demo/src/main/java/com/example/demo/cinema/controller/CinemaController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.cinema.service.CinemaService;
-
+/**
+ * Controller class for handling cinema-related HTTP requests.
+ */
 @CrossOrigin(origins= "http://localhost:3000")
 @RestController
 @RequestMapping("/cinema")
@@ -17,7 +19,11 @@ public class CinemaController {
 
     @Autowired
     private CinemaService cinemaService;
-
+    /**
+     * Endpoint to retrieve cinema-related data.
+     *
+     * @return A string containing cinema data.
+     */
     @GetMapping("/data")
     public String getCinemaData() {
         return cinemaService.getCinemaData();
--- Backend/Spring/demo/src/main/java/com/example/demo/cinema/service/EventService.java
+++ Backend/Spring/demo/src/main/java/com/example/demo/cinema/service/EventService.java
@@ -9,17 +9,25 @@ import com.example.demo.cinema.model.Event;
 
 import reactor.core.publisher.Mono;
 
-
+/**
+ * Service class for retrieving cinema events details using WebClient.
+ */
 @Service
 public class EventService {
-
+    // The URL of the cinema events API
     private final String apiUrl = "https://www.apollokino.ee/xml/Events";
-
+     /**
+     * Retrieve details of cinema events from the specified API.
+     *
+     * @return A Mono containing a list of Event objects.
+     */
     public Mono<List<Event>> getEventDetails() {
+        // Use WebClient to perform a GET request and retrieve events as a Flux
         return WebClient.create(apiUrl)
                 .get()
                 .retrieve()
                 .bodyToFlux(Event.class)
+                // Collect events into a list within a Mono
                 .collectList();
 
     }
